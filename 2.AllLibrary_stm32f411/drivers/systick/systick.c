ec49591d3ec93eb970b7499f0e5ba8c93b73ff6a
Hieu Le <lehieu10a13@gmail.com>
2026-09-06 07:45:54 +0700

    Fix makefile add link of lib

--- 2.AllLibrary_stm32f411/drivers/systick/systick.c
+++ 2.AllLibrary_stm32f411/drivers/systick/systick.c
@@ -2,11 +2,12 @@
 
 volatile uint32_t s_ticks = 0;
 void systick_init(uint32_t tick_hz){
-	uint32_t ticks = (tick_hz / 1000U);
-	SYSTICK -> RVR = ticks - 1;
-	SYSTICK -> CVR = 0;
-	SYSTICK -> CSR = BIT(0) |  BIT(1) | BIT(2); 
+	uint32_t ticks = (tick_hz / 1000U);		// 
+	SYSTICK -> RVR = ticks - 1;				// Set reload register
+	SYSTICK -> CVR = 0;						// Clear current value register
+	SYSTICK -> CSR = BIT(0) |  BIT(1) | BIT(2); 	// Enable Systick , use processor clock
 }
+// Systick interupt handler
 void SysTick_Handler(void)
 {
     ++s_ticks;
